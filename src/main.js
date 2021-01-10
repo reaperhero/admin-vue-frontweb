c9015038bc8cc49b7c7734b3d5509e37e84d30b3
chenqiangjun <chenqiangjun@mudu.tv>
2021-01-10 23:43:54 +0800

    userlist

--- src/main.js
+++ src/main.js
@@ -8,6 +8,15 @@ import './assets/css/global.css'
 
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 在request 拦截器中, 展示进度条 NProgress.start()
+// 请求在到达服务器之前，先会调用use中的这个回调函数来添加请求头信息
+axios.interceptors.request.use(config => {
+  console.log(config)
+  // 为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 在最后必须 return config
+  return config
+})
 // 挂在到Vue实例，后面可通过this调用
 Vue.prototype.$http = axios
 
