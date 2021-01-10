3e1eca20fb38388dab3dee75be26d19ad3688b8e
chenqiangjun <chenqiangjun@mudu.tv>
2021-01-10 18:34:59 +0800

    complete login

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 挂在到Vue实例，后面可通过this调用
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
